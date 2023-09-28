1acfacd32cb7bb867ab2322981da9c7f28408c74
LHennep <302468696@student.rocmondriaan.nl>
2023-09-28 14:24:17 +0200

    loops oefen

---
+++ js loops/main.js
@@ -0,0 +1,11 @@
+console.log("main.js is running...");
+
+let counter = 0;
+while (counter) {
+    console.log("counter is ", counter);
+
+    // counter = counter + 1;
+    //  counter += 1;
+    counter++;
+}
+console.log("einde loop");
