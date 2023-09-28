bf52247bd9dd9ba970c0acf589c5c4853553f741
LHennep <302468696@student.rocmondriaan.nl>
2023-09-28 15:10:07 +0200

    js loops bijna klaar

--- js loops/main.js
+++ js loops/main.js
@@ -1,7 +1,8 @@
 console.log("main.js is running...");
 
+// opdr 1
 let counter = 0;
-while (counter) {
+while (counter < 10) {
     console.log("counter is ", counter);
 
     // counter = counter + 1;
@@ -9,3 +10,100 @@ while (counter) {
     counter++;
 }
 console.log("einde loop");
+
+// opdr 2
+counter = 2;
+while (counter < 21) {
+    console.log("counter is ", counter);
+
+    counter++;
+}
+console.log("einde loop");
+
+// opdr 3
+counter = 10;
+while (counter > -1) {
+    console.log("counter is ", counter);
+
+    counter--;
+}
+console.log("einde loop");
+
+// opdr 4
+counter = 40;
+while (counter > 3) {
+    console.log("counter is ", counter);
+
+    counter--;
+}
+console.log("einde loop");
+
+// opdr 5
+counter = 8;
+while (counter > 0) {
+    console.log("Ik ben een student van ROC Mondriaan", counter);
+
+    counter--
+}
+console.log("einde loop");
+
+// opdr 6
+counter = 0;
+while (counter < 9) {
+    console.log("infinite loop", counter);
+
+    counter++
+    
+ 
+}
+console.log("einde loop");
+
+// opdr 7 
+counter = 0;
+while (counter < -2) {
+    console.log("counter is", counter);
+
+    counter++
+
+    if (counter >= 10){
+        break
+    }
+}
+
+console.log("einde loop");
+
+// opdr 8
+counter = 0;
+while (counter < 21) {
+    console.log("Loop", counter);
+
+    counter++
+
+    if (counter >= 11){
+        break
+    }
+}
+
+console.log("einde loop");
+
+// opdr 9
+counter = 0;
+while (counter < 21) {
+    console.log("Dit is zin 1. Dit is zin 2", counter);
+
+    counter++
+
+    if (counter >= 11){
+        console.log("dit is zin 2", counter);
+        continue
+        break
+
+    }
+    
+}
+
+console.log("einde loop");
+
+
+
+
