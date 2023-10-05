a6bb3c2ac52410dbee4a79e368f75e02edafd515
LHennep <302468696@student.rocmondriaan.nl>
2023-10-05 15:05:35 +0200

    js functions niet klaar

---
+++ JS functions/main.js
@@ -0,0 +1,44 @@
+console.log("main.js is running");
+
+// Opdr 1
+
+function Greet() {
+    console.log("hallo wereld");
+
+}
+
+Greet();
+
+// //Opdr 2
+
+function welcome() {
+   document.body.textContent = "Welkom op ROC Mondriaan"
+}
+
+welcome();
+
+//Opdr 3
+function calculateSum() {
+    console.log("Antwoord is", 3 + 4);
+}
+
+calculateSum()
+
+//Opdr 4
+function calculateDivision() {
+    console.log("Antwoord is", 8 / 2);
+}
+
+calculateDivision()
+
+// opdr 5
+function showArrayOfNumbers() {
+    console.log("Array Priemgetallen", [2, 3 ,5, 7, 11]);
+    
+}
+
+// opdr 6
+function showArrayOfBrands() {
+    console.log("Array automerken", ["Mercedes", "Volkswagen", "Peuget", "Tesla", "toyota"]);
+    
+}
