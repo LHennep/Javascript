443ce376a446cad3a80ff45c1042a7a1f9967fba
LHennep <302468696@student.rocmondriaan.nl>
2023-11-09 12:54:45 +0100

    oefenen

---
+++ Js oefenopdrachten/main.js
@@ -0,0 +1,13 @@
+console.log('main js is running');
+
+let salaris = 2000
+let verhoging = 1.05
+let antwoord = salaris * verhoging
+
+console.log("Opdracht is 1", antwoord);
+
+let prijs = 2000
+let percentage = 1.025
+ antwoord = prijs * percentage
+
+console.log("Opdracht is 1", antwoord);
